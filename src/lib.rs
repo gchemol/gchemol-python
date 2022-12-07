69944c0ffddcab453b5e92efc2a6146cbc5cfb57
Wenping Guo <ybyygu@gmail.com>
2022-12-07 19:13:13 +0800

    Set python module name as gchemol

--- src/lib.rs
+++ src/lib.rs
@@ -4,46 +4,22 @@ use pyo3::prelude::*;
 
 // [[file:../pygchemol.note::8fc5b8be][8fc5b8be]]
 use gchemol::prelude::*;
-use gchemol::Molecule;
+use gchemol::Molecule as GMolecule;
 
-#[pyclass(mapping, module = "pygchemol", subclass)]
-// #[pyo3(text_signature = "(/, multigraph=True, attrs=None)")]
+#[pyclass(mapping, module = "gchemol", subclass)]
 #[derive(Clone)]
-pub struct PyMolecule {
-    inner: Molecule,
-    // #[pyo3(get, set)]
-    // pub attrs: PyObject,
+pub struct Molecule {
+    inner: GMolecule,
 }
 
 #[pymethods]
-impl PyMolecule {
-    // #[new]
-    // // NOTE: set default python argument
-    // // #[args(multigraph = "true")]
-    // fn new(py: Python, attrs: Option<PyObject>) -> Self {
-    //     Self {
-    //         attrs: attrs.unwrap_or_else(|| py.None()),
-    //     }
-    // }
-
+impl Molecule {
     /// Construct `Molecule` object from a file `path`. If the file
     /// contains multiple molecules, only the last one will be read.
     #[staticmethod]
     fn from_file(py: Python, path: String) -> PyResult<Self> {
-        let inner = Molecule::from_file(&path)?;
+        let inner = GMolecule::from_file(&path)?;
         Ok(Self { inner })
-    }
-
-    /// Add a new node to the graph.
-    ///
-    /// :param obj: The python object to attach to the node
-    ///
-    /// :returns: The index of the newly created node
-    /// :rtype: int
-    #[pyo3(text_signature = "(self, obj, /)")]
-    fn add_atom(&mut self, obj: PyObject) -> PyResult<()> {
-        println!("add one node");
-        Ok(())
     }
 
     /// Dump molecule object in json format.
@@ -64,25 +40,34 @@ impl PyMolecule {
         Ok(self.inner.formula())
     }
 
-    /// Unbuild current crystal structure leaving a nonperiodic structure
-    fn unbuild_crystal(&self) -> PyResult<()> {
-        self.unbuild_crystal();
-        Ok(())
-    }
-
     /// Get the number of bonds.
     fn nbonds(&self) -> PyResult<usize> {
         let n = self.inner.nbonds();
         Ok(n)
+    }
+
+    /// Unbuild current crystal structure leaving a nonperiodic structure
+    fn unbuild_crystal(&mut self) -> PyResult<()> {
+        self.inner.unbuild_crystal();
+        Ok(())
+    }
+
+    /// Recalculates all bonds in molecule based on interatomic
+    /// distances and covalent radii. For periodic system, the bonds
+    /// are determined by applying miniumu image convention.
+    fn rebond(&mut self) -> PyResult<()> {
+        self.inner.rebond();
+        Ok(())
     }
 }
 // 8fc5b8be ends here
 
 // [[file:../pygchemol.note::ec553356][ec553356]]
 #[pymodule]
+#[pyo3(name = "gchemol")]
 fn pygchemol(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    m.add_class::<PyMolecule>()?;
+    m.add_class::<Molecule>()?;
 
     Ok(())
 }
